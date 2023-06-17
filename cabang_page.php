41ae2bd0ddbc6fa58ce1a145982eaee1e0de6df0
Zhafir Zidan <gerhana.total753@gmail.com>
2023-06-17 21:53:09 +0700

    sak sake

---
+++ cabang_page.php
@@ -0,0 +1,94 @@
+<?php
+session_start();
+include('includes/header.php');
+include('includes/navbar.php');
+require_once './includes/db.php';
+?>
+
+<div class="container-fluid">
+
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold">Data Cabang
+            </h6>
+        </div>
+
+        <div class="card-body">
+
+            <div class="table-responsive" id="tabel-content">
+
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead style="background: #E33035; color: white;">
+                        <tr>
+                            <th>No</th>
+                            <th>Nama Cabang</th>
+                            <th>Alamat</th>
+                            <th>Kontak</th>
+                            <th>EDIT</th>
+                            <th>DELETE</th>
+                        </tr>
+                    </thead>
+                    <tbody style="color: black;">
+                        <?php
+                        if (isset($_GET['search'])) {
+                            $search = ($_GET['search']);
+                            $editor = mysqli_query($conn, "select * from editor where name like '%" . $search . "%'");
+                        } else {
+                            $editor = mysqli_query($conn, "Select * from editor");
+                        }
+
+                        foreach ($editor as $rows) {
+                        ?>
+                            <tr>
+                                <td>
+                                    <?= $rows['no'] ?>
+                                </td>
+                                <td>
+                                    <?= $rows['nama_cabang'] ?>
+                                </td>
+                                <td>
+                                    <?= $rows['alamat'] ?>
+                                </td>
+                                <td>
+                                    <?= $rows['kontak'] ?>
+                                </td>
+                                <td>
+                                    <form action="" method="post">
+                                        <input type="hidden" name="edit_id" value="">
+                                        <button type="submit" name="edit_btn" class="btn btn-success"> EDIT</button>
+                                    </form>
+                                </td>
+                                <td>
+                                    <form action="" method="post">
+                                        <input type="hidden" name="delete_id" value="">
+                                        <button type="submit" name="delete_btn" class="btn btn-danger"> DELETE</button>
+                                    </form>
+                                </td>
+                            <?php
+                        }
+                            ?>
+                            </tr>
+
+                    </tbody>
+                </table>
+
+            </div>
+            <div style="display: inline-block;padding-right: 1rem">
+                <button style="height: 50px; font-weight: bold;padding:0 1.5rem" class="btn btn-warning "><img src="img/mdi_edit-box.svg" alt="">
+                    Tambah Data</button>
+            </div>
+            <div style="display: inline-block">
+                <button style="height: 50px; font-weight: bold;" class="btn btn-danger "><img src="img/pVector.svg" alt="">
+                    Export
+                    PDF</button>
+            </div>
+        </div>
+    </div>
+
+</div>
+<!-- /.container-fluid -->
+<?php
+include('includes/scripts.php');
+include('includes/footer.php');
+?>
--- includes/export.php
+++ includes/export.php
@@ -7,43 +7,42 @@ session_start();
 $level = $_SESSION["level"];
 
 if (isset($_GET['exportTabel']) && ($level == "100" || $level == "2")) {
-   $export = $_GET['exportTabel'];
-   if ($export == 'karyawan' ) {
+  $export = $_GET['exportTabel'];
+  if ($export == 'karyawan') {
     // intance object dan memberikan pengaturan halaman PDF
-        $pdf=new FPDF('P','mm','A4');
-        $pdf->AddPage();
-        
-        $pdf->SetFont('Times','B',13);
-        $pdf->Cell(200,10,'DATA KARYAWAN',0,0,'C');
-        
-        $pdf->Cell(10,15,'',0,1);
-        $pdf->SetFont('Times','B',9);
-        $pdf->Cell(10,7,'NO',1,0,'C');
-        $pdf->Cell(50,7,'NAMA' ,1,0,'C');
-        $pdf->Cell(75,7,'ALAMAT',1,0,'C');
-        $pdf->Cell(55,7,'EMAIL',1,0,'C');
-        
-        
-        $pdf->Cell(10,7,'',0,1);
-        $pdf->SetFont('Times','',10);
-        $no=1;
-                    if(isset($_GET['search'])){
-              $search = ($_GET['search']);
-              $data = mysqli_query($conn, "select * from editor where name like '%".$search."%'");
-            }else{
-              $data = mysqli_query($conn,"Select * from editor");
-            }
-        while($d = mysqli_fetch_array($data)){
-        $pdf->Cell(10,6, $no++,1,0,'C');
-        $pdf->Cell(50,6, $d['id_admin'],1,0);
-        $pdf->Cell(75,6, $d['name'],1,0);  
-        $pdf->Cell(55,6, $d['username'],1,1);
-        }
-        
-        $pdf->Output();
-   }
+    $pdf = new FPDF('P', 'mm', 'A4');
+    $pdf->AddPage();
 
-}   else{
-    header("Location:../index.php");
-   }
+    $pdf->SetFont('Times', 'B', 13);
+    $pdf->Cell(200, 10, 'DATA KARYAWAN', 0, 0, 'C');
+
+    $pdf->Cell(10, 15, '', 0, 1);
+    $pdf->SetFont('Times', 'B', 9);
+    $pdf->Cell(10, 7, 'NO', 1, 0, 'C');
+    $pdf->Cell(50, 7, 'NAMA', 1, 0, 'C');
+    $pdf->Cell(75, 7, 'ALAMAT', 1, 0, 'C');
+    $pdf->Cell(55, 7, 'EMAIL', 1, 0, 'C');
+
+
+    $pdf->Cell(10, 7, '', 0, 1);
+    $pdf->SetFont('Times', '', 10);
+    $no = 1;
+    if (isset($_GET['search'])) {
+      $search = ($_GET['search']);
+      $data = mysqli_query($conn, "select * from editor where name like '%" . $search . "%'");
+    } else {
+      $data = mysqli_query($conn, "Select * from editor");
+    }
+    while ($d = mysqli_fetch_array($data)) {
+      $pdf->Cell(10, 6, $no++, 1, 0, 'C');
+      $pdf->Cell(50, 6, $d['id_admin'], 1, 0);
+      $pdf->Cell(75, 6, $d['name'], 1, 0);
+      $pdf->Cell(55, 6, $d['username'], 1, 1);
+    }
+
+    $pdf->Output();
+  }
+} else {
+  header("Location:../index.php");
+}
 ?>
